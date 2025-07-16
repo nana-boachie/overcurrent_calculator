b1d987935e2945f8102ed38d66b454232115d1c2
nana-boachie <nkyboachie@gmail.com>
2025-07-16 14:17:21 +0000

    Refactored and removed the constant initialization

--- main.c
+++ main.c
@@ -5,10 +5,13 @@
 
 
 int main(void){
+
     double pickup_current, fault_current, time_multiplier_setting;
+    
     int curve_type, standard;
 
     printf("Overcurrent Protection Trip Time Calculator\n");
+    
     printf("========================================\n");
  
     // Prompt user for curve standard
@@ -23,19 +26,20 @@ int main(void){
 
 
 
-  printf("Enter Pickup Current (A): ");
-  scanf("%lf", &pickup_current);
-  printf("Enter Fault Current (A): ");
-  scanf("%lf", &fault_current);
-  printf("Enter Time Multiplier Setting: ");
-  scanf("%lf", &time_multiplier_setting);
+    printf("Enter Pickup Current (A): ");
+    scanf("%lf", &pickup_current);
+    printf("Enter Fault Current (A): ");
+    scanf("%lf", &fault_current);
+    printf("Enter Time Multiplier Setting: ");
+    scanf("%lf", &time_multiplier_setting);
 
-  if (standard == IEC)
-  {
+
+    if (standard == IEC)
+    {
       printf("Select IEC Curve Type (0: Inverse, 1: Very Inverse, 2: Extremely Inverse, 3: Long Time Standard Inverse): ");
       scanf("%d", &curve_type);
       calculate_trip_time_iec(pickup_current, fault_current, time_multiplier_setting, curve_type);
-  } else if (standard == IEEE) {
+    } else if (standard == IEEE) {
         printf("Select IEEE Curve Type (4: Moderate Inverse, 5: Very Inverse, 6: Extremely Inverse): ");
         scanf("%d", &curve_type);
         calculate_trip_time_ieee(pickup_current, fault_current, time_multiplier_setting, curve_type);
--- overcurrent_calculator.c
+++ overcurrent_calculator.c
@@ -8,9 +8,9 @@
 
 
 int calculate_trip_time_iec(double pickup_current,double fault_current,
-    double time_multiplier_setting,int IECCurveType)
-{
-    double trip_time;
+    double time_multiplier_setting,int IECCurveType){
+    
+        double trip_time;
     printf("Calculating trip time for IEC standard...\n");
     printf("Pickup Current: %.2f A, Fault Current: %.2f A, Time Multiplier Setting: %.2f\n",
            pickup_current, fault_current, time_multiplier_setting);
@@ -24,33 +24,24 @@ int calculate_trip_time_iec(double pickup_current,double fault_current,
     switch (IECCurveType) 
     {
     case 0: // Inverse
-    double k = 0.14; // Example constant for Inverse curve
-    double a = 0.02;
-    trip_time = (k / (pow((pickup_current / fault_current),a)-1)) * time_multiplier_setting;
+    trip_time = (.14 / (pow((pickup_current / fault_current),.02)-1)) * time_multiplier_setting;
     printf("Trip time (Inverse): %.2f seconds\n", trip_time);
     break;
     case 1: // Very Inverse
-    k = 13.5; // Example constant for Very Inverse curve
-    a = 1;
-    trip_time = (k / (pow((pickup_current / fault_current), a) - 1)) * time_multiplier_setting;
+    trip_time = (13.5 / (pow((pickup_current / fault_current), 1) - 1)) * time_multiplier_setting;
     printf("Trip time (Very Inverse): %.2f seconds\n", trip_time);
     break;
- case 2: //Extremely Inverse
-    a = 2;
-    trip_time = (80 / (pow((pickup_current / fault_current), a) - 1)) * time_multiplier_setting;
+    case 2: //Extremely Inverse
+    trip_time = (80 / (pow((pickup_current / fault_current), 2) - 1)) * time_multiplier_setting;
     printf("Trip time (Extremely Inverse): %.2f seconds\n", trip_time);
     break;
  case 3: // Long time standard inverse
-    k = 120; // Example constant for Very Inverse curve
-    a = 1;
-    trip_time = (k / (pow((pickup_current / fault_current), a) - 1)) * time_multiplier_setting;
+    trip_time = (120/ (pow((pickup_current / fault_current), 1) - 1)) * time_multiplier_setting;
     printf("Trip time (Long time standard inverse): %.2f seconds\n", trip_time);
     break;
     default:
     printf("Invalid IEC curve type.\n");
     return -2; // Error code for invalid curve type
-    
-     
     }
 
     return 0;
